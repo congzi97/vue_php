205bd4d7c3677d2d8f6d88b21fe759b6b50e9702
congzi97 <544301197@qq.com>
2017-09-24 08:13:51 -0500

    Update config.php

--- php/config/config.php
+++ php/config/config.php
@@ -37,9 +37,9 @@ return  [
         // SMTP 服务器地址
         'smtpHost' => 'smtp.163.com',
         // SMTP服务器用户名
-        'userName' => 'notexpired@163.com',
+        'userName' => '',
         // 邮箱密码或授权码
-        'password' => 'congzi970407',
+        'password' => '',
         // 发送人名称
         'fromName' => 'CZPhp',
         // 发送人的邮箱账号
